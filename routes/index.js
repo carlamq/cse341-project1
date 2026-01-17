8214cfc49af77a0f3aebbb334094d7ce7cb7e6a3
CarlaMQ <carlaquintanar1907@gmail.com>
2026-01-18 01:02:19 +0900

    get

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router(); //alows to handle routs
 
 router.get('/', (req, res) => { res.send('Hello There') });
 
+router.use('/contacts', require('./contacts'));
+
 module.exports = router;
